7ebaea31b6e88b9177f2e4712713b49b9885248d
Greg Morgan <gregm9999@gmail.com>
2018-11-25 20:44:15 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,18 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+
+    num_1 = Kernel.rand(1..20)
+num_2 = Kernel.rand(1..20)
+num_3 = Kernel.rand(1..20)
+
+if num_1==num_2 && num_2==num_3
+  puts "You Win"
+else
+  puts "You Lose"
+end
+
+
     resp.write "Hello, World"
     resp.finish
   end
