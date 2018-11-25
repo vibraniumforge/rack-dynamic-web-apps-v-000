bed4e31d52112f793c2cfdfa2316ae947fa5e5a1
Greg Morgan <gregm9999@gmail.com>
2018-11-25 20:46:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,9 +13,9 @@ class Application
     resp.write "#{num_3}\n"
 
     if num_1==num_2 && num_2==num_3
-      puts "You Win"
+      resp.write "You Win"
     else
-      puts "You Lose"
+      resp.write "You Lose"
     end
 
     resp.finish
