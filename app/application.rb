465564e48d96367a6b91fb8c4be98e23cfaf6f70
Greg Morgan <gregm9999@gmail.com>
2018-11-25 20:45:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,14 +4,19 @@ class Application
     resp = Rack::Response.new
 
     num_1 = Kernel.rand(1..20)
-num_2 = Kernel.rand(1..20)
-num_3 = Kernel.rand(1..20)
+    num_2 = Kernel.rand(1..20)
+    num_3 = Kernel.rand(1..20)
 
-if num_1==num_2 && num_2==num_3
-  puts "You Win"
-else
-  puts "You Lose"
-end
+    
+    resp.write "#{num_1}\n"
+    resp.write "#{num_2}\n"
+    resp.write "#{num_3}\n"
+
+    if num_1==num_2 && num_2==num_3
+      puts "You Win"
+    else
+      puts "You Lose"
+    end
 
     resp.finish
   end
